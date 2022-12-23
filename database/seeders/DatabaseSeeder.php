0a38113304d7e4f08cfa8189d725ba5e40932939
Victoriasilvagit <victoriadoesantosilva@gmail.com>
2022-12-23 15:37:51 -0300

    Create DatabaseSeeder.php

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Database\Seeders\ProjectSeeder;
+use Database\Seeders\TeacherSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            ProjectSeeder::class,
+            TeacherSeeder::class
+        ]);
+    }
+}
